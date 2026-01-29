eb80be369598ed78eace3d2ba37aff057cb455a7
connect <connect.pohl.py@gmail.com>
2026-01-29 11:33:28 -0300

    agregue el react router y cree la pagina de login

--- connect/src/App.jsx
+++ connect/src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
-import './App.css'
+import React from 'react';
+import { useState } from 'react';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LandingPage from './pages/landing.jsx'
+import './App.css';
 
 function App() {
   return (
-    <section className="App">
-      <LandingPage />
-    </section>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </Router>
   )
 }
 
---
+++ connect/src/pages/login.jsx
@@ -0,0 +1,6 @@
+function login() {
+  return (
+    <div>Login Page</div>
+  )
+}   
+export default login
